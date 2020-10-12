ec36d1f2ee885d3d429c84ec47e07ab037e28f83
brandontiu <tiubrandon@hotmail.com>
2020-10-11 20:24:33 -0400

    init

---
+++ Assets/Scripts/DropperScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropperScript : MonoBehaviour {
+    public Rigidbody rb;
+    public float moveSpeed = 2.0f;
+    public GameObject coin;
+    public GameObject leftwall;
+    private float leftWallXcoord;
+    public GameObject rightwall;
+    private float rightWallXcoord;
+
+    // Start is called before the first frame update
+    void Start() {
+        // get the Rigidbody and the coords for the walls
+        rb = this.GetComponent<Rigidbody>();
+        leftWallXcoord = leftwall.transform.position.x;
+        rightWallXcoord = rightwall.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // restrict the dropper to be between the two walls
+        Vector3 currentPosition = this.transform.position;
+        currentPosition.x = Mathf.Clamp(currentPosition.x, 
+            leftWallXcoord, rightWallXcoord);
+        this.transform.position = currentPosition;
+
+        // get the position of the input on the x-axis and
+        // turn it into a vector
+        float x = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(x, 0, 0);
+
+        // define the velocity of movement
+        rb.velocity = direction * moveSpeed;
+
+        // if user wants to drop a coin
+        if (Input.GetKeyDown("space")) {
+            Instantiate(coin, this.transform.position, this.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/PusherScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PusherScript : MonoBehaviour {   
+    public Vector3 initPosition;
+    public Vector3 newPosition;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        // get the initial position and Rigidbody
+        initPosition = this.transform.position;
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // update the pushers position
+        newPosition = new Vector3(initPosition.x, initPosition.y, 
+            initPosition.z + Mathf.Sin(Time.time));
+
+        // move the pusher to the new position 
+        this.rb.MovePosition(newPosition);
+    }
+}
